79e194d5f34196e6ba84a3bfc865a9dfdecdfd2a
Fernando Trujano <trujano@mit.edu>
2014-10-15 14:57:13 -0400

    Add more specs

--- src/warmup/Pingball.java
+++ src/warmup/Pingball.java
@@ -13,6 +13,13 @@ public interface Pingball {
  * Gadgets:
  * Every gadget has an (x,y) location, where x and y are integers in [0,19].
  * Every gadget has a width and height, also integers in [0,19]Others are configurable
- * Some gadgets have a coefficient of reflection,
+ * Some gadgets have a coefficient of reflection, this is a multiplier applied to the ball's 
+ *      velocity after it bounces off the gadget. Default multiplier is 1.0. 
+ * Some gadgets have an orientation - how the gadget is rotated from its default orientation. 
+ *      This is given in degrees (clockwise). Default orientation is 0. 
+ * Some gadgets have triggers - an event that happens at the gadget. 
+ * Some gadgets have actions - A response that a gadget can make to a trigger happening somewhere on the board. 
  */
+    
+    
 }
