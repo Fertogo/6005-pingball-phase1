440f8ba0981c8a1a26e121c331bf6dd49fb5e167
Martin Martinez <wisefool@mit.edu>
2014-10-15 14:15:40 -0400

    Adding specs

---
+++ src/warmup/Pingball.java
@@ -0,0 +1,18 @@
+package warmup;
+
+public interface Pingball {
+
+/*
+ * Playing Area:
+ * playing area must be 20 L wide by 20 L high. That is, 400 square bumpers
+ * The upper left corner is (0,0) and the lower right corner is (20,20).
+ */
+
+
+/*
+ * Gadgets:
+ * Every gadget has an (x,y) location, where x and y are integers in [0,19].
+ * Every gadget has a width and height, also integers in [0,19]Others are configurable
+ * Some gadgets have a coefficient of reflection,
+ */
+}
