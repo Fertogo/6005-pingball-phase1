a42ce7cef2aea8d69a18a23871cd19eb7870e4c4
Fernando Trujano <trujano@mit.edu>
2014-10-17 14:00:55 -0400

    Gadget interface skeleton

---
+++ src/phase1/Gadget.java
@@ -0,0 +1,86 @@
+package phase1; 
+import java.awt.Rectangle;
+
+import physics.*; 
+/**
+ * 
+ * TODO: Specs for the interface
+ *
+ */
+public interface Gadget {
+
+    
+    /**
+     * TODO: Write specs
+     * @param x
+     * @param y
+     * @param orientation
+     * @param size
+     * @return
+     */
+    public static Gadget absorber(int x, int y, double orientation,Rectangle size){ 
+        return new Absorber(x,  y,  orientation,size); 
+    }
+    
+    /**
+     * TODO: Write specs
+     * @param x
+     * @param y
+     * @return
+     */
+    public static Gadget squareBumper(int x, int y){ 
+        return new SquareBumper(x,  y, size); 
+    }
+    
+    /**
+     * TODO: Write specs
+     * @param x
+     * @param y
+     * @param orientation
+     * @return
+     */
+    public static Gadget triangleBumper(int x, int y, double orientation){ //TODO: Size and shape
+        return new TriangleBumper(x,  y, size); 
+    }
+    
+    /**
+     * TODO: Write specs
+     * @param x
+     * @param y
+     * @return
+     */
+    public static Gadget circleBumper(int x, int y){ 
+        return new CircleBumper(x,  y, size); 
+    }
+    
+    /**
+     * TODO: Write specs
+     * @param type (left or right)
+     * @return
+     */
+    public static Gadget flipper(int x, int y, String type){ 
+        return new Flipper(x,  y, size); 
+    }
+    
+    /**
+     * TODO: Write specs
+     * @param wall
+     * @return
+     */
+    public static Gadget outerWall(LineSegment wall){ 
+        return new OuterWall(wall); 
+    }
+    
+    
+    /**
+     * TODO: Write specs
+     */
+    public void trigger(); 
+    
+    /**
+     * TODO: Write specs
+     */
+    public void action(Gadget gadget); 
+
+    
+}
--- src/warmup/Main.java
+++ src/warmup/Main.java
@@ -16,7 +16,7 @@ public class Main {
         Board pingBoard = new Board(20,20);
         //Create ball
         Vect velocity = new Vect(3.0, 3.0);
-        Ball pingBall = new Ball(1,10, velocity);
+        Ball pingBall = new Ball(3,10, velocity);
 
         //While (True)
 //        System.out.print(pingBoard.toString());
