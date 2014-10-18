e3f617e810573b2815f7325c1717ebaf0e0db6a0
martin-martinez <wisefool@mit.edu>
2014-10-18 15:48:26 -0400

    Created the Absorber and Flipper classes, just the skeleton for now

---
+++ src/phase1/Absorber.java
@@ -0,0 +1,14 @@
+package phase1;
+
+import java.awt.Rectangle;
+
+public class Absorber {
+    
+    
+    public Absorber(int x, int y, double orientation,Rectangle size){ 
+        
+    }
+    
+    
+}
+    
---
+++ src/phase1/Flipper.java
@@ -0,0 +1,8 @@
+package phase1;
+
+public class Flipper {
+    
+    public Flipper(int x, int y, String type){ 
+        
+    }
+}
