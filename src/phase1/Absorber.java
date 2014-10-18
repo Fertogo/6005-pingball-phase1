85c2351a79e10984042f0fc5c81ea50ab1d9cb98
martin-martinez <wisefool@mit.edu>
2014-10-18 16:23:47 -0400

    Added basic description of class to both Absorber and Flipper

--- src/phase1/Absorber.java
+++ src/phase1/Absorber.java
@@ -3,7 +3,17 @@ package phase1;
 import java.awt.Rectangle;
 
 public class Absorber {
-    
+    /*
+     * Size and Shape: A rectangle kL *mL where k and m are 0<k<=20
+     * Orientation: Only one kind
+     * Coef of Reflection: None, ball captured
+     * Trigger: generated when the ball hits it 
+     * Action: shoots out store ball
+     * Description: absorber stops ball and holds it. Ball's center is .25L from bottom .25 from the right side,
+     * Initial velocity of the ball should be 50L/sec with default gravity and default values for friction
+     * if not holding the ball or if previously rejected ball hos not left the absorber, 
+     * then the absorber takes no action when triggered.   
+     */
     
     public Absorber(int x, int y, double orientation,Rectangle size){ 
         
--- src/phase1/Flipper.java
+++ src/phase1/Flipper.java
@@ -1,7 +1,19 @@
 package phase1;
 
 public class Flipper {
-    
+/*
+ * Size and Shape: Rectangular rotating shape with bounding box 2Lx2L
+ * 
+ * Orientation: 
+ * for left- 0degrees, for pivot point in northwest corner, counterclockwise
+ * right   - pivot point in northeast corner, clockwise
+ * 
+ * Coeff of Reflection: .95
+ * Trigger - when ball hits it
+ * Action-rotates 90 degrees, angular velocity of 1080; account for the linear 
+ * velocity of the part that contacts the ball
+ * 
+ */
     public Flipper(int x, int y, String type){ 
         
     }
