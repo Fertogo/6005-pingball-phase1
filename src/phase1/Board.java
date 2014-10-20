0efd5bb0d478946e58c498d1dde114ac439de427
Fernando Trujano <trujano@mit.edu>
2014-10-19 22:30:00 -0400

    Board toString()

--- src/phase1/Board.java
+++ src/phase1/Board.java
@@ -1,5 +1,8 @@
 package phase1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     private Gadget[] gadgets; 
     private int height; 
@@ -19,8 +22,42 @@ public class Board {
         //checkRep()
     }
     
+    /**
+     * 
+     * @param x - x position in board
+     * @param y - y position in board
+     * @return - A char representation of the topmost character when layers are merged. 
+     */
+    private char mergeBoard( List<String> layers, int x, int y){ 
+//        List<String> layers = new ArrayList<String>(); 
+//        for (Gadget gadget : this.gadgets){ 
+//            layers.add(gadget.toString(this.width,this.height)); 
+//        }
+        
+        for (String layer : layers){ 
+            String[] lines = layer.split("\n"); 
+            if (lines[y].charAt(x) == ' '){ 
+                continue; 
+            }
+            else return lines[y].charAt(x); 
+        }
+        return ' '; 
+    }
+    
     @Override
-    public String toString(){ 
+    public String toString(){
+
+        
+        //Merge Layers
+        StringBuilder board = new StringBuilder(""); 
+        for (int y=0; y < height-1; y++){ 
+            for (int x=0; x<width-1; x++){        
+                //Current position = x,y 
+                board.append(mergeBoard(x,y)); 
+            }
+            board.append("\n"); 
+        }
+        return ""; 
         //Idea 1
                //Make empty array of size (width, height)
                // Add walls (see OuterWalls)
